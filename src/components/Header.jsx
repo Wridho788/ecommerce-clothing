86f2260f5ae7b73a21be34e9d5b4b8d30a1a0a1c
wRidho788 <wridho246@gmail.com>
2023-10-15 22:46:45 +0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
+
+const Header = () => {
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  return (
+    <div>
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)}>open/close Sidebar</div>
+    </div>
+  );
+};
+
+export default Header;
